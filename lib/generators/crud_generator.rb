10cdd121e18fdbd41f80f0f73fa0788d567f1c65
ProGM <progiemmeh@gmail.com>
2022-09-29 16:47:44 +0200

    :new: Tested and tuned for the sample project

--- lib/generators/crud_generator.rb
+++ lib/generators/crud_generator.rb
@@ -1,18 +1,19 @@
-require 'rails'
+require "rails"
 
 class CrudGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('templates', __dir__)
+  source_root File.expand_path("templates", __dir__)
 
-  argument :attributes, type: :array, default: [], banner: 'attributes'
+  argument :attributes, type: :array, default: [], banner: "attributes"
 
   check_class_collision
 
+  class_option :authenticated, type: :boolean, default: false
   class_option :parent, type: :string, desc: "The parent class for the generated model"
 
   desc "This generator creates a migration"
   def create_migration_file
     raw_attrs = attributes.map { |attr| "#{attr.name}:#{attr.type}" }
-    generate 'migration', "Create#{class_name.pluralize.classify}", *raw_attrs, migration_name: "create_#{table_name}"
+    generate "migration", "Create#{class_name.pluralize.classify}", *raw_attrs, migration_name: "create_#{table_name}"
   end
 
   desc "This generator creates a model"
@@ -27,42 +28,56 @@ class CrudGenerator < Rails::Generators::NamedBase
 
   desc "this generates the factory bot model"
   def create_factory_file
-    template "rspec/factory_bot/factory_bot.rb.tt", "spec/factories/#{class_name.gsub("::", "").pluralize.underscore}.rb"
+    template "rspec/factory_bot/factory_bot.rb.tt",
+             "spec/factories/#{class_name.gsub("::", "").pluralize.underscore}.rb"
   end
 
   desc "This generator creates a controller"
   def create_controller_file
-    template "active_record/controller/api_controller.rb.tt", "app/controllers/api/v1/#{table_name}_controller.rb"
+    template "active_record/controller/api_controller.rb.tt", "app/controllers/v1/#{table_name}_controller.rb"
   end
 
   desc "This generates the rswag test file"
   def create_rswag_file
-    template "rspec/request/request_spec.rb.tt", "spec/requests/v1/#{file_name}_spec.rb"
+    if options["authenticated"]
+      template "rspec/request/authenticated_request_spec.rb.tt", "spec/requests/v1/#{file_name}_spec.rb"
+    else
+      template "rspec/request/request_spec.rb.tt", "spec/requests/v1/#{file_name}_spec.rb"
+    end
   end
 
   desc "This generator creates a serializer"
   def create_serializer_file
-    template "active_record/serializer/serializer.rb.tt", "app/serializers/#{file_name}.rb"
+    template "active_record/serializer/serializer.rb.tt", "app/serializers/#{file_name}_serializer.rb"
   end
 
+  desc "This generator declares the model in cancancan"
+  def create_ability_file
+    template "cancancan/ability/ability.rb.tt", "app/models/ability.rb" unless File.exist?("app/models/ability.rb")
+    spy = if options["authenticated"]
+            "def define_authenticated_abilities!(user)\n"
+          else
+            "def initialize(user)\n"
+          end
+
+    code = "    can :manage, #{class_name}\n"
+
+    inject_into_file "app/models/ability.rb", code, after: spy, verbose: false, force: false
+  end
+
+  desc "This generator defines a resource"
   def create_routes_file
-    # TODO: Aggiungere alle rotte il api_resources
+    route "api_resources :#{file_name.pluralize}", namespace: ["v1"] + regular_class_path
   end
 
   private
 
   def permitted_params
-    attributes.map { |attr| ":#{attr.name}" }.join(',')
+    attributes.map { |attr| ":#{attr.name}" }.join(",")
   end
 
   def parent_class_name
-    if parent
-      parent
-    # elsif database
-    #   abstract_class_name
-    else
-      "ApplicationRecord"
-    end
+    parent || "ApplicationRecord"
   end
 
   def parent
--- monade_rails_generators.gemspec
+++ monade_rails_generators.gemspec
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-$:.push File.expand_path("../lib", __FILE__)
+
+$:.push File.expand_path("lib", __dir__)
 
 require "monade/rails/generators/version"
 require "monade_rails_generators"
@@ -12,17 +13,17 @@ Gem::Specification.new do |spec|
 
   spec.summary       = "Monade Rails generators"
   spec.description   = "Monade Rails generators."
-  spec.homepage      = "https://github.com/monade/monade_rails_generators"
+  spec.homepage      = "https://gitlab.com/monade-srl/monade_rails_generators"
   spec.license       = "MIT"
-  spec.required_ruby_version = ">= 2.6.0"
+  spec.required_ruby_version = ">= 2.7.0"
 
   spec.metadata["homepage_uri"] = spec.homepage
   spec.metadata["source_code_uri"] = spec.homepage
-  spec.metadata["changelog_uri"] = "https://github.com/monade/monade_rails_generators/CHANGELOG.md"
+  spec.metadata["changelog_uri"] = "https://gitlab.com/monade-srl/monade_rails_generators/CHANGELOG.md"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
-  spec.files = Dir['lib/**/*']
+  spec.files = Dir["lib/**/*"]
   # spec.files = Dir.chdir(File.expand_path(__dir__)) do
   #   `git ls-files -z`.split("\x0").reject { |f| f.match(%r{\A(?:test|spec|features)/}) }
   # end
@@ -31,7 +32,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib", "lib/monade/rails/generators"]
 
   # Uncomment to register a new dependency of your gem
-  # spec.add_dependency "example-gem", "~> 1.0"
+  spec.add_dependency "rails", "~> 7.0"
 
   # For more information and examples about making a new gem, checkout our
   # guide at: https://bundler.io/guides/creating_gem.html
